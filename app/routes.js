e270a9d3ff45e1ed552b110fd39f3cc00adc6ed7
Teffen Ellis <teffen@nirri.us>
2017-11-02 02:51:44 +0900

    Flesh out separation of account and web property.

--- app/routes.js
+++ app/routes.js
@@ -34,76 +34,117 @@ module.exports = function setRoutes (app) {
       return
     }
 
+    function handleAPIError (error) {
+      console.error('API Error', error)
+
+      response.json({
+        proceed: false,
+        errors: [{type: '400', message: error.toString()}]
+      })
+    }
+
     oauth2Client.setCredentials({
       access_token: request.body.authentications.account.token.token
     })
 
-    analytics.management.accountSummaries.list({}, (error, accountSummaries) => {
-      if (error) {
-        response.json({
-          proceed: false,
-          errors: [{type: '400', message: error.toString()}]
-        })
-        return
-      }
+    const fetchAccountSummaries = new Promise((resolve, reject) => {
+      analytics.management.accountSummaries.list({}, (error, accountSummaries) => {
+        if (error) return reject(error)
 
-      const analyticsEntries = []
-      const enumNames = {}
-
-      // TODO: Provision the account if one hasn't been created.
-      if (!accountSummaries || !accountSummaries.items) {
-        response.json({
-          proceed: false,
-          errors: [{type: '400', message: 'User does not have a Google Analytics account.'}]
-        })
-        return
-      }
-
-      accountSummaries.items.forEach(item => {
-        item.webProperties.forEach(properties => {
-          analyticsEntries.push({
-            name: properties.name,
-            id: properties.id
-          })
-        })
+        return resolve(accountSummaries)
       })
-
-      if (!analyticsEntries.length) {
-        response.json({proceed: true})
-        return
-      }
-
-      // Populate install schema with user's analytic IDs
-      analyticsEntries.forEach(entry => {
-        enumNames[entry.id] = `${entry.name} (${entry.id})`
-      })
-
-      Object.assign(install.schema.properties.id, {
-        enum: analyticsEntries.map(entry => entry.id),
-        enumNames
-      })
-
-      install.options.id = analyticsEntries[0].id
-
-      response.json({install})
     })
+
+    // const createAccount = new Promise((resolve, reject) => {
+    //   console.log('createAccount')
+    // })
+
+    const checkAnalyticsAccount = new Promise((resolve, reject) => {
+      analytics.management.accounts.list({}, (error, accounts) => {
+        if (error) {
+          // if (error.code === 403) return resolve(createAccount)
+          return reject(error)
+        }
+
+        return resolve(accounts)
+      })
+    })
+
+    checkAnalyticsAccount
+      .catch(handleAPIError)
+      .then(() => fetchAccountSummaries)
+      .catch(handleAPIError)
+      .then(accountSummaries => {
+        const accountIds = []
+        const accountIdNames = {}
+        const webPropertyPrefix = 'web-properties-for-'
+
+        // Populate each account as an "Organization".
+        accountSummaries.items.forEach((account, index) => {
+          accountIds.push(account.id)
+          accountIdNames[account.id] = account.name
+        })
+
+        Object.assign(install.schema.properties.organization, {
+          enum: accountIds,
+          enumNames: accountIdNames,
+          default: accountIds[0]
+        })
+
+        install.options.organization = accountIds[0]
+
+        // Populate each organization's web properties.
+        const {webPropertyTemplate} = install.schema.properties
+        const namePattern = /\$ORGANIZATION/g
+
+        accountSummaries.items.forEach((account, index) => {
+          const schemaName = webPropertyPrefix + account.id
+          const webPropertyIds = []
+          const webPropertyNames = {}
+
+          account.webProperties.forEach(property => {
+            webPropertyIds.push(property.id)
+            webPropertyNames[property.id] = property.name
+          })
+
+          install.schema.properties[schemaName] = Object.assign({}, webPropertyTemplate, {
+            showIf: {
+              organization: account.id
+            },
+            type: 'string',
+            title: webPropertyTemplate.title.replace(namePattern, account.name),
+            order: webPropertyTemplate.order + index,
+            default: webPropertyIds[0],
+            enum: webPropertyIds,
+            enumNames: webPropertyNames
+          })
+
+          install.options[schemaName] = webPropertyIds[0]
+        })
+
+        webPropertyTemplate.type = 'hidden'
+
+        response.json({install})
+      })
+      .catch(handleAPIError)
   })
 
   // "item-add"
-  app.post('/provision-account', function (request, response) {
-    const {install} = request.body
+  app.post('/provision-property', function (request, response) {
+    const {install, metadata} = request.body
 
+    console.log(request.body)
     oauth2Client.setCredentials({
       access_token: request.body.authentications.account.token.token
     })
 
-    const account =
+    // const account =
 
     analytics.management.webproperties.insert({
       accountId: '123456',
       resource: {
-        websiteUrl: 'http://www.examplepetstore.com',
-        name: 'Example Store'
+        websiteUrl: metadata.item.url,
+        name: metadata.item.name
       }
     })
 
